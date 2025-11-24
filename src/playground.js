3cfed3e96044cb7ca1c78f6b73e6674786d0b58b
Evelin Cobos <evecobos8@gmail.com>
2025-11-24 10:59:56 -0500

    short-response completed

---
+++ src/playground.js
@@ -0,0 +1,46 @@
+
+const inventoryManager = {
+  storeName: 'BestBuy',
+  items: ['computer', 'TV'],
+
+  addItem(newItem) {
+    this.items.push(newItem)
+   
+  },
+
+  printInventory() {
+    this.items.forEach(item => {
+      console.log(`${this.storeName} has ${item}`)
+    })
+  }
+}
+
+console.log(inventoryManager.addItem('Laundry Machine'))
+console.log(inventoryManager.printInventory())
+
+
+
+
+
+class Animal {
+  speak() {
+    console.log('Animal sound')
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log('Woof')
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log('Meow')
+  }
+}
+
+const dog = new Dog()
+console.log(dog.speak())
+
+
